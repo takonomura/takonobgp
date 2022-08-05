f31022030a9014793c11d1bb2f7fcdac7a96cac0
takonomura <takonomura@users.noreply.github.com>
2022-08-05 17:24:18 +0900

    Update for test in tinet

--- main.go
+++ main.go
@@ -7,21 +7,21 @@ import (
 	"strconv"
 )
 
-func getenv(name string) string {
+func getenvOrDefault(name, def string) string {
 	v := os.Getenv(name)
 	if v == "" {
-		log.Fatalf("%s is not specified", name)
+		return def
 	}
 	return v
 }
 
 func main() {
-	myAS, err := strconv.ParseUint(getenv("MY_ASN"), 10, 16)
+	myAS, err := strconv.ParseUint(getenvOrDefault("MY_ASN", "65001"), 10, 16)
 	if err != nil {
 		log.Fatalf("parsing MY_ASN: %v", err)
 	}
 
-	routerID := net.ParseIP(getenv("ROUTER_ID"))
+	routerID := net.ParseIP(getenvOrDefault("ROUTER_ID", "10.0.0.1"))
 	if routerID == nil || len(routerID) != 4 {
 		log.Fatalf("invalid ROUTER_ID")
 	}
@@ -33,5 +33,5 @@ func main() {
 		ID:       id,
 		HoldTime: 180,
 	}
-	log.Fatal(p.connect("tcp", getenv("NEIGHBOR_ADDR")))
+	log.Fatal(p.connect("tcp", getenvOrDefault("NEIGHBOR_ADDR", "10.0.0.2")))
 }
