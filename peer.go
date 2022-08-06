1ec1d0f7be5fdda28d02a298ceb4894aa3eaf8f6
takonomura <takonomura@users.noreply.github.com>
2022-08-07 08:48:08 +0900

    Send update message

--- peer.go
+++ peer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 )
 
 type Peer struct {
@@ -58,6 +59,7 @@ func (p *Peer) connect(network, address string) error {
 		return fmt.Errorf("send keepalive message: %w", err)
 	}
 
+	var sendUpdateOnce sync.Once
 	for {
 		log.Printf("receiving message")
 
@@ -71,6 +73,24 @@ func (p *Peer) connect(network, address string) error {
 			log.Printf("received keepalive message")
 			if err := p.sendMessage(KeepaliveMessage{}); err != nil {
 				return fmt.Errorf("send pong: %w", err)
+			}
+			// 相手から keepalive が帰ってきて established になるはずなので、初回の keepalive 後に UPDATE を送る
+			sendUpdateOnce.Do(func() {
+				log.Printf("sending update message")
+				_, route, _ := net.ParseCIDR("10.1.0.0/24") // TODO: Configurable
+				err = p.sendMessage(UpdateMessage{
+					PathAttributes: []PathAttribute{
+						OriginAttributeIGP.ToPathAttribute(),
+						ASPathAttribute{Sequence: true, Segments: []uint16{p.MyAS}}.ToPathAttribute(),
+						NextHopAttribute(p.ID[:]).ToPathAttribute(),
+					},
+					NLRI: []*net.IPNet{
+						route,
+					},
+				})
+			})
+			if err != nil {
+				return fmt.Errorf("send update message: %w", err)
 			}
 		default:
 			log.Printf("received message: %T (%+v)", m, m)
