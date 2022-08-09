22c63ac9b0384b07fe86cb8544f75c617d8160c6
takonomura <takonomura@users.noreply.github.com>
2022-08-10 08:41:38 +0900

    Move UpdateMessageToRIBEntries to update_message.go

--- rib.go
+++ rib.go
@@ -132,64 +132,6 @@ func (rib *RIB) Update(e *RIBEntry) error {
 	return nil
 }
 
-func UpdateMessageToRIBEntries(m UpdateMessage, source *Peer) ([]*RIBEntry, error) {
-	var (
-		origin  Origin
-		asPath  ASPath
-		nextHop NextHop
-		others  []PathAttribute
-
-		mpReach MPReachNLRI
-		//mpUnreach MPUnreachNLRI
-
-		err error
-	)
-
-	for _, a := range m.PathAttributes {
-		switch a.TypeCode {
-		case AttributeTypeOrigin:
-			origin, err = OriginFromPathAttribute(a)
-		case AttributeTypeASPath:
-			asPath, err = ASPathFromPathAttribute(a)
-		case AttributeTypeNextHop:
-			nextHop, err = NextHopFromPathAttribute(a)
-		case AttributeTypeMPReachNLRI:
-			mpReach, err = MPReachNLRIFromPathAttribute(a)
-		//case AttributeTypeMPUnreachNLRI:
-		//        mpUnreach, err = MPUnreachNLRIFromPathAttribute(a)
-		default:
-			others = append(others, a)
-		}
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	entries := make([]*RIBEntry, 0, len(mpReach.NLRI)+len(m.NLRI))
-
-	for _, r := range mpReach.NLRI {
-		entries = append(entries, &RIBEntry{
-			Prefix:          r,
-			Origin:          origin,
-			ASPath:          asPath,
-			NextHop:         mpReach.NextHop[0],
-			OtherAttributes: others,
-			Source:          source,
-		})
-	}
-	for _, r := range m.NLRI {
-		entries = append(entries, &RIBEntry{
-			Prefix:          r,
-			Origin:          origin,
-			ASPath:          asPath,
-			NextHop:         net.IP(nextHop),
-			OtherAttributes: others, // TODO: Copy other attributes?
-			Source:          source,
-		})
-	}
-	return entries, nil
-}
-
 func (rib *RIB) Entries() []*RIBEntry {
 	rib.mutex.RLock()
 	defer rib.mutex.RUnlock()
