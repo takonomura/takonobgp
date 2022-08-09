42c64a35cf22c8b1302ff3d1a0c8de3c9d359703
takonomura <takonomura@users.noreply.github.com>
2022-08-09 14:18:46 +0900

    Add debug message

--- iproute.go
+++ iproute.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"os/exec"
+	"strings"
 )
 
 func ipRoute(args ...string) error {
+	log.Printf("execute: ip route %s", strings.Join(args, " "))
 	cmd := exec.Command("ip", append([]string{"route"}, args...)...)
 	result, err := cmd.CombinedOutput()
 	if err != nil {
