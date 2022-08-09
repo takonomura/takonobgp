ec81a32220812467d07c81c82c42e0ecad3cb1b8
takonomura <takonomura@users.noreply.github.com>
2022-08-09 18:38:44 +0900

    Implement MP-BGP path attributes

---
+++ mp.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
+type (
+	AFI  uint16
+	SAFI uint8
+)
+
+const (
+	AttributeTypeMPReachNLRI   AttributeTypeCode = 14
+	AttributeTypeMPUnreachNLRI AttributeTypeCode = 15
+
+	AFIIPv4     AFI  = 1
+	AFIIPv6     AFI  = 2
+	SAFIUnicast SAFI = 1
+)
+
+func NLRISize(afi AFI, safi SAFI) int {
+	switch {
+	case afi == AFIIPv4 && safi == SAFIUnicast:
+		return 32
+	case afi == AFIIPv6 && safi == SAFIUnicast:
+		return 128
+	default:
+		return 0
+	}
+}
+
+type MPReachNLRI struct {
+	AFI     AFI
+	SAFI    SAFI
+	NextHop net.IP
+	NLRI    []*net.IPNet
+}
+
+func MPReachNLRIFromPathAttribute(a PathAttribute) (MPReachNLRI, error) {
+	if a.TypeCode != AttributeTypeMPReachNLRI {
+		return MPReachNLRI{}, fmt.Errorf("invalid type code: %d", a.TypeCode)
+	}
+	if len(a.Value) < 4 {
+		return MPReachNLRI{}, fmt.Errorf("invalid length: %d", len(a.Value))
+	}
+	v := MPReachNLRI{
+		AFI:     AFI(binary.BigEndian.Uint16(a.Value[0:2])),
+		SAFI:    SAFI(a.Value[2]),
+		NextHop: net.IP(a.Value[4 : 4+a.Value[3]]),
+	}
+	r := bytes.NewReader(a.Value[4+a.Value[3]:])
+
+	for r.Len() > 0 {
+		route, err := readIPNet(r, NLRISize(v.AFI, v.SAFI))
+		if err != nil {
+			return MPReachNLRI{}, fmt.Errorf("nlri: %w", err)
+		}
+		v.NLRI = append(v.NLRI, route)
+	}
+
+	return v, nil
+}
+
+func (a MPReachNLRI) ToPathAttribute() PathAttribute {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, uint16(a.AFI))
+	buf.Write([]byte{uint8(a.SAFI), uint8(len(a.NextHop))})
+	buf.Write([]byte(a.NextHop))
+	for _, r := range a.NLRI {
+		writeIPNet(buf, r)
+	}
+
+	return PathAttribute{
+		Flags:    0b10000000, // optional non-transitive
+		TypeCode: AttributeTypeMPReachNLRI,
+		Value:    buf.Bytes(),
+	}
+}
+
+type MPUnreachNLRI struct {
+	AFI             AFI
+	SAFI            SAFI
+	WithdrawnRoutes []*net.IPNet
+}
+
+func MPUnreachNLRIFromPathAttribute(a PathAttribute) (MPUnreachNLRI, error) {
+	if a.TypeCode != AttributeTypeMPUnreachNLRI {
+		return MPUnreachNLRI{}, fmt.Errorf("invalid type code: %d", a.TypeCode)
+	}
+	if len(a.Value) < 3 {
+		return MPUnreachNLRI{}, fmt.Errorf("invalid length: %d", len(a.Value))
+	}
+	v := MPUnreachNLRI{
+		AFI:  AFI(binary.BigEndian.Uint16(a.Value[0:2])),
+		SAFI: SAFI(a.Value[2]),
+	}
+	r := bytes.NewReader(a.Value[3:])
+
+	for r.Len() > 0 {
+		route, err := readIPNet(r, NLRISize(v.AFI, v.SAFI))
+		if err != nil {
+			return MPUnreachNLRI{}, fmt.Errorf("withdrawn: %w", err)
+		}
+		v.WithdrawnRoutes = append(v.WithdrawnRoutes, route)
+	}
+
+	return v, nil
+}
+
+func (a MPUnreachNLRI) ToPathAttribute() PathAttribute {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, uint16(a.AFI))
+	buf.Write([]byte{uint8(a.SAFI)})
+	for _, r := range a.WithdrawnRoutes {
+		writeIPNet(buf, r)
+	}
+
+	return PathAttribute{
+		Flags:    0b10000000, // optional non-transitive
+		TypeCode: AttributeTypeMPUnreachNLRI,
+		Value:    buf.Bytes(),
+	}
+}
