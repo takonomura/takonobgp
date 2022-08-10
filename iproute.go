f8d8af3809bd1a1c3375df9a852023e4228c7e6d
takonomura <takonomura@users.noreply.github.com>
2022-08-10 11:07:32 +0900

    Print message before error

--- iproute.go
+++ iproute.go
@@ -10,11 +10,11 @@ func ipRoute(args ...string) error {
 	log.Printf("execute: ip route %s", strings.Join(args, " "))
 	cmd := exec.Command("ip", append([]string{"route"}, args...)...)
 	result, err := cmd.CombinedOutput()
-	if err != nil {
-		return err
-	}
 	if len(result) > 0 {
 		log.Printf("ip route: %s", string(result))
+	}
+	if err != nil {
+		return err
 	}
 	return nil
 }
